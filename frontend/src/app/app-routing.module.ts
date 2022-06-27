45c6c3613d9665833c8cbc35a4734fdb2894867e
Pablo <pablokintopp@gmail.com>
2022-06-27 08:12:02 -0300

    Create task view component template and styles

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
