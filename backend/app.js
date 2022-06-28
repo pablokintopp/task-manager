ffa024c477b950e98b19b0c9e57b10dbca379807
Pablo <pablokintopp@gmail.com>
2022-06-28 08:20:46 -0300

    CRUD Tasks

--- backend/app.js
+++ backend/app.js
@@ -46,6 +46,44 @@ app.delete('/lists/:id', (request, response) => {
     });
 });
 
+app.get('/lists/:listId/tasks', (request, response) => {
+    Task.find({ _listId: request.params.listId }).then((tasks) => {
+        response.send(tasks);
+    });
+});
+
+app.post('/lists/:listId/tasks', (request, response) => {
+    let newTask = new Task({
+        title: request.body.title,
+        _listId: request.params.listId
+    });
+
+    newTask.save().then((newTaskDocument) => {
+        response.send(newTaskDocument);
+    });
+});
+
+app.patch('/lists/:listId/tasks/:taskId', (request, response) => {
+    Task.findOneAndUpdate({
+        _id: request.params.taskId,
+        _listId: request.params.listId
+    },
+        { $set: request.body }).then(() => {
+            response.sendStatus(200);
+        });
+});
+
+app.delete('/lists/:listId/tasks/:taskId', (request, response) => {
+
+    Task.findOneAndDelete({
+        _id: request.params.taskId,
+        _listId: request.params.listId
+    }).then((removedListDocument) => {
+        response.send(removedListDocument);
+    });
+
+});
+
 app.listen(APPLICATION_PORT, () => {
     console.log("Server is listening on port " + APPLICATION_PORT);
 });
