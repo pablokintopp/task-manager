9aec8b55a7b1a65ed7f01968c58cfd5979a605e5
Pablo <pablokintopp@gmail.com>
2022-06-28 09:08:37 -0300

    ENABLE CORS

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,12 @@ const APPLICATION_PORT = 3000;
 
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (request, response) => {
     response.send("Hello World!");
 });
