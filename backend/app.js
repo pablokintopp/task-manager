d3de40ee554ecdb423a81ce6d8fb93963e2c7bfd
Pablo <pablokintopp@gmail.com>
2022-07-05 10:49:19 -0300

    Update cors policy on backend

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,14 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE"
   );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    HEADER_ACCESS_TOKEN_NAME,
+    HEADER_REFRESH_TOKEN_NAME,
+  ]);
 
   res.header("Access-Control-Expose-Headers", [
     HEADER_ACCESS_TOKEN_NAME,
