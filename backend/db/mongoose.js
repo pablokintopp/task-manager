5371e4e19c3f748ca6117f9c0c3ac9b9c8304634
Boyquotes <webmaster@linux-live-cd.org>
2023-05-25 11:26:38 +0000

    Docker for server

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
 
-//const DB_URL = 'mongodb://root:example@0.0.0.0:27017/TaskManager';
-//const DB_URL = 'mongodb://root:example@localhost:27017/TaskManager';
-//const DB_URL = 'mongodb://root:example@mongo/TaskManager';
-//const DB_URL = 'mongodb://mongo/TaskManager';
-//const DB_URL = 'mongodb://localhost:27017/TaskManager';
-//const DB_URL = 'mongodb://127.0.0.1:27017/TaskManager';
-const DB_URL = 'mongodb://127.0.0.1:27777/TaskManager';
+const DB_URL = 'mongodb://root:example@mongo/TaskManager';
 
 mongoose.Promise = global.Promise;
 
